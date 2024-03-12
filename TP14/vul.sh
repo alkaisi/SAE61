c6690a9930ed8cbea94fc2e7988e23ed08b5594e
user <user@debian>
2024-03-12 15:47:36 +0100

    v1

--- TP14/vul.sh
+++ TP14/vul.sh
@@ -3,28 +3,37 @@
 # Chemin vers le fichier de sortie
 OUTPUT_FILE="/home/user/SAE61/TP14/debsecan_vul.txt"
 
-# Suite Debian à analyser
-DEBIAN_SUITE="bookworm"
+# Seuil de vulnérabilités
+SEUIL=10
 
-# Seuil d'alerte pour le nombre de CVE
-THRESHOLD=10
+# Adresse e-mail de destination
+EMAIL_DESTINATION="ibrahimalkaisi.iut@gmail.com"
 
-# Fonction pour envoyer une alerte par e-mail
-send_email_alert() {
-    local subject="Alerte : Dépassement du seuil de CVE"
-    local body="Le nombre de CVE a dépassé le seuil autorisé. Veuillez vérifier le fichier $OUTPUT_FILE pour plus de détails."
-    echo "$body" | mail -s "$subject" ibrahimalkaisi.iut@gmail.com
-}
+# Serveur SMTP
+SMTP_SERVER="smtp.gmail.com"
+SMTP_PORT="587"
+SMTP_USERNAME="ibrahimalkaisi.iut@gmail.com"
+SMTP_PASSWORD="J'ai essayé mais cela ne fonctionne pas, il prend bcp de temps mais pas de résultats, par contre il génére le fichier de scan"
+
+# Chemin complet vers ssmtp
+SSMTP_PATH="/usr/sbin/ssmtp"
 
 # Exécuter debsecan et enregistrer la sortie dans le fichier
-/usr/sbin/debsecan --suite="$DEBIAN_SUITE" --format detail > "$OUTPUT_FILE"
+debsecan --suite=bookworm --format detail >> "$OUTPUT_FILE"
 
-# Vérifier si le nombre de CVE dépasse le seuil
-num_cve=$(grep -c "CVE-" "$OUTPUT_FILE")
-if [ "$num_cve" -gt "$THRESHOLD" ]; then
-    send_email_alert
+# Nombre de vulnérabilités trouvées
+NUM_VUL=$(grep -c 'vulnerability' "$OUTPUT_FILE")
+
+# Envoyer un e-mail d'alerte si le nombre de vulnérabilités dépasse le seuil
+if [ "$NUM_VUL" -gt "$SEUIL" ]; then
+    SUBJECT="Alerte de sécurité - Nombre de vulnérabilités dépasse le seuil"
+    BODY="Le nombre de vulnérabilités trouvées ($NUM_VUL) dépasse le seuil autorisé ($SEUIL). Veuillez vérifier le fichier $OUTPUT_FILE pour plus de détails."
+
+    # Envoyer l'e-mail en utilisant ssmtp avec le chemin complet
+    echo -e "Subject:$SUBJECT\n$BODY" | "$SSMTP_PATH" -v -au"$SMTP_USERNAME" -ap"$SMTP_PASSWORD" "$EMAIL_DESTINATION"
 fi
 
-# Ajouter des entrées dans le journal avec un horodatage
-echo "$(date '+%Y-%m-%d %H:%M:%S') : Analyse de sécurité debsecan exécutée" >> /var/log/debsecan.log
+# Historiser les résultats dans un fichier log
+LOG_FILE="/home/user/SAE61/TP14/debsecan_log.txt"
+echo "$(date): Nombre de vulnérabilités trouvées - $NUM_VUL" >> "$LOG_FILE"
 
